e5d2896dce3fa4e462788efb858650479d3ea572
LFallenAngel <45042273+LFallenAngel@users.noreply.github.com>
2019-01-16 19:49:09 +0100

    Paneles Animaciones Programadas

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+
+    public Animator animMenuPrincipal;
+    public Animator animMenuSeleccion;
+    public Animator animInfo;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animMenuSeleccion.SetBool("activar",false);
+        animMenuPrincipal.SetBool("desActivar", false);
+        animInfo.SetBool("activar", false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NoMuestraMenu()
+    {
+        animMenuPrincipal.SetBool("desActivar",true);
+    }
+    public void MuestraMenu()
+    {
+        animMenuPrincipal.SetBool("desActivar", false);
+        animInfo.SetBool("activar", false);
+        animMenuSeleccion.SetBool("activar", false);
+    }
+
+
+    public void MuestraInfo()
+    {
+        animInfo.SetBool("activar",true);
+        animMenuPrincipal.SetBool("desActivar", false);
+
+    }
+
+    public void MuestraMenuSeleccion()
+    {
+        animMenuSeleccion.SetBool("activar", true);
     }
 }
