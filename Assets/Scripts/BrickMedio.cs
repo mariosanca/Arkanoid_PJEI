0fa0cfc13bf4b6afddc3cb8fdfdc7fe6761b25de
Mario <mario7.yt@gmail.com>
2019-01-23 19:46:03 +0100

    #37 #38 Programar bricks simple y medio
    Script de los brick simple y medio creados

---
+++ Assets/Scripts/BrickMedio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickMedio : MonoBehaviour
+{
+
+    int vida = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vida <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("bola"))
+        {
+            vida--;
+        }
+    }
+}
